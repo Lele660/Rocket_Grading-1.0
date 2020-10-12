3d9f0320d0156d3e51112de69f5b3d8f45034899
Lele660 <60441401+Lele660@users.noreply.github.com>
2020-10-12 14:29:43 -0400

    initalized project

---
+++ src/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javafx.application.Application;
+
+/**
+ *
+ * @author lelezhao
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Application.launch(Login.class,args);
+    
+    }
+    
+}
