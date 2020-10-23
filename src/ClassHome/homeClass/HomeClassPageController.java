6e8a208f2aedc1268f7e20acc44a2db1ee79a761
Vanessa Di Maso <72408379+vanessaDiM@users.noreply.github.com>
2020-10-23 15:02:24 -0400

    Add files via upload

---
+++ src/ClassHome/homeClass/HomeClassPageController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pages.homeClass;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author vanes
+ */
+public class HomeClassPageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    Button signOut;
+    @FXML
+    Button Home;
+    
+    public void signOut (ActionEvent event) throws SQLException, IOException {
+        Parent lRoot = FXMLLoader.load(getClass().getResource("/pages/login/login.fxml"));
+        Scene lScene = new Scene(lRoot);
+        Stage secondaryStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        secondaryStage.setTitle("Sign In");
+        secondaryStage.setScene(lScene);
+        secondaryStage.show();
+        pages.files.info.logOut();
+    }
+    public void home (ActionEvent event) throws SQLException, IOException {
+        Parent hRoot = FXMLLoader.load(getClass().getResource("/pages/homePage/home.fxml"));
+        Scene hScene = new Scene(hRoot);
+        Stage tertiaryStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        tertiaryStage.setTitle("Home");
+        tertiaryStage.setScene(hScene);
+        tertiaryStage.show();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
