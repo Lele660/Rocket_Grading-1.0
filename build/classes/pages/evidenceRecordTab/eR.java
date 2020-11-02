55bde7bd2e13f73e8c030f65003789075fbf0bce
Vanessa Di Maso <72408379+vanessaDiM@users.noreply.github.com>
2020-11-02 16:06:52 -0500

    Add files via upload

---
+++ build/classes/pages/evidenceRecordTab/eR.java
@@ -0,0 +1,31 @@
+/*
+ * opens evidence record tab page 
+ * mainly used for testing
+ */
+package pages.evidenceRecordTab;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author vanessa
+ */
+public class eR extends Application{
+    public static void main(String[] args) {
+       launch(args);
+    }
+     @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Evidence Record Tab");
+        Parent homeRoot = FXMLLoader.load(getClass().getResource("eRTabPage.fxml"));
+        Scene homeScene = new Scene(homeRoot);
+        primaryStage.setScene(homeScene);
+        primaryStage.show();
+    }
+    
+}
